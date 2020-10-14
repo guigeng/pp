640f09e567ce664d12958fa50097fabe1762f055
root <root@proxy6.walsin.com>
2020-10-14 13:35:18 +0800

    branch clark

---
+++ README.md
@@ -0,0 +1,13 @@
+# proxypool
+
+本项目是 proxypool 的个人自用修改版，由于原作者经常删库，故在此备份以免代码失传。我修改的版本在 [master](https://github.com/ClarkeCheng/proxypool/tree/master) 分支，原版在 [master_old](https://github.com/ClarkeCheng/proxypool/tree/master_old) 分支，需要请自取。
+
+[我修改的与原版的差异](https://github.com/ClarkeCheng/proxypool/compare/master_old...ClarkeCheng:master)
+
+## 部署
+
+可用 Docker Compose 快速部署我修改的版本，配置文件见 [docker-compose.yml](https://github.com/ClarkeCheng/proxypool/blob/master/docker-compose.yml) 。
+
+## 更新日志
+
+- 2020 年 9 月 10 日更新至 v0.3.10
---
+++ pkg/proxy/geoip.go
@@ -0,0 +1,105 @@
+package proxy
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+
+	"github.com/oschwald/geoip2-golang"
+	bingeoip "github.com/zu1k/proxypool/internal/bindata/geoip"
+)
+
+var geoIp GeoIP
+
+func InitGeoIpDB() {
+	err := bingeoip.RestoreAsset("", "assets/GeoLite2-City.mmdb")
+	if err != nil {
+		panic(err)
+	}
+	err = bingeoip.RestoreAsset("", "assets/flags.json")
+	if err != nil {
+		panic(err)
+	}
+	geoIp = NewGeoIP("assets/GeoLite2-City.mmdb", "assets/flags.json")
+}
+
+// GeoIP2
+type GeoIP struct {
+	db       *geoip2.Reader
+	emojiMap map[string]string
+}
+
+type CountryEmoji struct {
+	Code  string `json:"code"`
+	Emoji string `json:"emoji"`
+}
+
+// new geoip from db file
+func NewGeoIP(geodb, flags string) (geoip GeoIP) {
+	// 判断文件是否存在
+	_, err := os.Stat(geodb)
+	if err != nil && os.IsNotExist(err) {
+		log.Println("文件不存在，请自行下载 Geoip2 City库，并保存在", geodb)
+		os.Exit(1)
+	} else {
+		db, err := geoip2.Open(geodb)
+		if err != nil {
+			log.Fatal(err)
+		}
+		geoip.db = db
+	}
+
+	_, err = os.Stat(flags)
+	if err != nil && os.IsNotExist(err) {
+		log.Println("flags 文件不存在，请自行下载 flags.json，并保存在", flags)
+		os.Exit(1)
+	} else {
+		data, err := ioutil.ReadFile(flags)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		var countryEmojiList = make([]CountryEmoji, 0)
+		err = json.Unmarshal(data, &countryEmojiList)
+		if err != nil {
+			log.Fatalln(err.Error())
+			return
+		}
+
+		emojiMap := make(map[string]string)
+		for _, i := range countryEmojiList {
+			emojiMap[i.Code] = i.Emoji
+		}
+		geoip.emojiMap = emojiMap
+	}
+	return
+}
+
+// find ip info
+func (g GeoIP) Find(ipORdomain string) (ip, country string, err error) {
+	ips, err := net.LookupIP(ipORdomain)
+	if err != nil {
+		return "", "", err
+	}
+	ip = ips[0].String()
+
+	var record *geoip2.City
+	record, err = g.db.City(ips[0])
+	if err != nil {
+		return
+	}
+	countryIsoCode := record.Country.IsoCode
+	if countryIsoCode == "" {
+		country = fmt.Sprintf("ZZ")
+	}
+	_, found := g.emojiMap[countryIsoCode]
+	if found {
+		country = fmt.Sprintf("%v", countryIsoCode)
+	} else {
+		country = fmt.Sprintf("ZZ")
+	}
+	return
+}
